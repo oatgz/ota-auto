06a7df7225cfaa75028bd00ffc00d202ec0a7575
oatgz <oatgzs@gmail.com>
2017-06-29 14:13:46 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
 echo "Hello Ota-auto";
+$api_key="rxKOV3hat_e_6-di58LIAy8XB238Xihp";
+$connect = 'https://api.mlab.com/api/1/databases/ota/collections/lineota?apiKey='.$api_key.'';
+$connect=new Mongo();
+$db = $connect->selectDB( "ota" );
+$collection = $db->selectCollection( "otaline" );
+$cursor = $collection->find();
+   foreach ($cursor as $document) {
+      echo $document["question"] . "\n";
+   }
