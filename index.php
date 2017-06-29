78b3fb850c1d4f2dc835a71a34979575555312a5
oatgz <oatgzs@gmail.com>
2017-06-29 15:20:21 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
 <?php
-try
-{
-    $connection = new Mongo('mongodb://oatgz:Oatgz123456@ds135812.mlab.com:35812/ota');
-	echo $connection;
-    $database   = $connection->selectDB('ota');
-	echo $database;
-    $collection = $database->selectCollection('otaline');
-	echo $collection;
-}
-catch(MongoConnectionException $e)
-{
-    die("Failed to connect to database ".$e->getMessage());
-}
-
-$cursor = $collection->find();
-
+echo "คำถาม กับ ota-auto";
 ?>
