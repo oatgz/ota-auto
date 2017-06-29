7caa006087600daf009663e710f5d7e95e41bef8
oatgz <oatgzs@gmail.com>
2017-06-29 14:29:16 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 try
 {
     $connection = new Mongo('mongodb://oatgz:Oatgz123456@ds135812.mlab.com:35812/ota');
+	echo $connection;
     $database   = $connection->selectDB('ota');
+	echo $database;
     $collection = $database->selectCollection('otaline');
+	echo $collection;
 }
 catch(MongoConnectionException $e)
 {
