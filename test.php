be5df71c4a7fd49aef9d6aa9062a3de51a88c400
oatgz <oatgzs@gmail.com>
2017-09-14 13:14:53 +0700

    Add files via upload

--- test.php
+++ test.php
@@ -7,7 +7,7 @@ $uri = "mongodb://".$uname.":".$pword."@ds135812.mlab.com:35812/ota";
 
 $conn = new MongoDB\Client($uri);
 
-echo "uri: " . $uri . "<br>";
-echo "conn: " . $conn . "<br>";
+echo "uri: ". $uri . "<br>";
+echo "conn: ". $conn . "<br>";
 
 ?>
