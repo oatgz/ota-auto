30faeccd0ebf0dfe17670c435f7c385a61f9faa5
oatgz <oatgzs@gmail.com>
2017-09-14 13:07:05 +0700

    Add files via upload

--- test.php
+++ test.php
@@ -1,12 +1,12 @@
 <?php
 
-
+echo "คำถาม กับ ota-auto";
 $uname = "oatgz";
 $pword = "Oatz123456";
 $uri = "mongodb://".$uname.":".$pword."@ds135812.mlab.com:35812/ota";
 
 $conn = new MongoDB\Client($uri);
 
-echo $uri
+
 
 ?>
